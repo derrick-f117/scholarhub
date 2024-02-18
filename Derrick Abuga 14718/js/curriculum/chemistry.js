96a672569147e5a155b9827f0fa1303a66911bf7
Derrick <152125688+derrick-f117@users.noreply.github.com>
2024-02-18 05:11:59 -0800

    Modified

---
+++ Derrick Abuga 14718/js/curriculum/chemistry.js
@@ -0,0 +1,18 @@
+// Get form elements
+const form1 = document.getElementById("formIchem");
+const form2 = document.getElementById("formIIchem");
+
+// Get textarea elements
+const ansI1 = getElementById("questionI1");
+const ansI2 = getElementById("questionI2");
+const ansII1 = getElementById("questionII1");
+const ansII2 = getElementById("questionII2");
+
+// Event listener for checking answers
+form1.addEventListener(function () {
+    // get usr's answr
+    const usrAnsI1 = ansI1.value.trim();
+    const usrAnsI2 = ansI2.value.trim();
+    const usrAnsII1 = ansII1.value.trim();
+    const usrAnsII2 = ansII2.value.trim();
+})
